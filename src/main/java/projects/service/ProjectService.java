1329b0ec41a3f4817b2ed42b1ae3f794fd0567df
kawanars <153693747+kawanars@users.noreply.github.com>
2024-05-19 21:07:31 -0400

    Add files via upload

---
+++ src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,32 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+
+public class ProjectService {
+	
+	
+	private ProjectDao projectDao = new ProjectDao();
+
+	public Project addProject(Project project) {
+
+		return projectDao.insertProject(project);
+	}
+	public List<Project> fetchAllProjects() {
+
+		return ProjectDao.fetchAllProjects();
+		
+	}
+
+	public Project fetchProjectById(Integer projectId) {
+	    return projectDao.fetchProjectById(projectId)
+	        .orElseThrow(() -> new NoSuchElementException(
+	            "Project with project id=" + projectId + " does not exist"));
+	}
+
+}
+	
